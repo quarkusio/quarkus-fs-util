19ccf6d74d24feb6286ef7489052e4c21b9e0d8a
Martin Panzer <postremus1996@googlemail.com>
2021-12-15 22:53:58 +0100

    Load FileSystemProviders with system cl

--- src/main/java/io/quarkus/fs/util/FileSystemProviders.java
+++ src/main/java/io/quarkus/fs/util/FileSystemProviders.java
@@ -9,11 +9,19 @@ public class FileSystemProviders {
     public static final FileSystemProvider ZIP_PROVIDER;
     static {
         FileSystemProvider foundZipProvider = null;
-        for (FileSystemProvider installedProvider : FileSystemProvider.installedProviders()) {
-            if (installedProvider.getScheme().equals("jar")) {
-                foundZipProvider = installedProvider;
-                break;
+        final ClassLoader ccl = Thread.currentThread().getContextClassLoader();
+        try {
+            // We are loading "installed" FS providers that are loaded from the system classloader anyway
+            // To avoid potential ClassCastExceptions we are setting the context classloader to the system one
+            Thread.currentThread().setContextClassLoader(ClassLoader.getSystemClassLoader());
+            for (FileSystemProvider installedProvider : FileSystemProvider.installedProviders()) {
+                if (installedProvider.getScheme().equals("jar")) {
+                    foundZipProvider = installedProvider;
+                    break;
+                }
             }
+        } finally {
+            Thread.currentThread().setContextClassLoader(ccl);
         }
         ZIP_PROVIDER = foundZipProvider;
     }
